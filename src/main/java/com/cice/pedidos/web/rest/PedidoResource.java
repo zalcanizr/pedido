530ca69b7ce6b1d2301ad548cbddac1f30d1381e
cice <zalcaniz.r@gmail.com>
2018-11-14 21:30:22 +0100

    last modified

--- src/main/java/com/cice/pedidos/web/rest/PedidoResource.java
+++ src/main/java/com/cice/pedidos/web/rest/PedidoResource.java
@@ -1,18 +1,30 @@
 package com.cice.pedidos.web.rest;
 
 
+import com.cice.pedidos.service.IPedidoService;
+import com.cice.pedidos.web.dto.PedidoDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/)")
+import java.util.List;
+
+@RestController
+
+@RequestMapping(path="/pedidos")
 public class PedidoResource {
 
-
+//    @Autowired
+//    IPedidoService pedidoService;
 
     void getPedidoById(){
 
     }
-    void getAllPedidosByUser(String Usuario){
-
+    @RequestMapping (method= RequestMethod.GET, path ="/{id}")
+    public String getAllPedidosByUser(@PathVariable (name ="id") String id){
+        return "Pedidos mostrados.";
     }
     void crearPedidoByUser(long idUsuario, String usuario){
 
